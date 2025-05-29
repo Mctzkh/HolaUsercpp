f0e3bbb175c09f55a86532271f02620f28233983
Mctzkh <marlonmarro0912@gmail.com>
2025-05-29 09:27:40 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     string user;
-    
+    //saquen al umul
     cout<<"Ingrese su nombre "<<endl;
-
+    
     cin>>user;
 
     if (user != "" || user != " "){
